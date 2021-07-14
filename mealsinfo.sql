486f0c465f63ce59d0dbd8f410a49c9e841b528c
h3m7T <39868208+hemanthsoma@users.noreply.github.com>
2021-07-14 13:26:38 +0530

    Update mealsinfo.sql

--- mealsinfo.sql
+++ mealsinfo.sql
@@ -6,29 +6,29 @@ SELECT * FROM MealsInfo limit 1;
 
 -- To select limited rows from table
 
-SELECT * FROM table limit 5;
+SELECT * FROM table_info limit 5;
 
 -- Delete
 
 DELETE from table
-SELECT * FROM table WHERE meal_id = 1438;
+SELECT * FROM table_info WHERE meal_id = 1438;
 
 
 -- Update
 
-UPDATE table
+UPDATE table_info
 SET category = "Salad" WHERE meal_id = 2307;
-SELECT * FROM table;
+SELECT * FROM table_info;
 
 -- Insert
 
-INSERT into table values(1755, "Beverages", "Thai");
-SELECT * FROM table;
+INSERT into table_info values(1755, "Beverages", "Thai");
+SELECT * FROM table_info;
 
 -- Drop a table
 
-INSERT into table values(2640,"Starters","Thai");
-SELECT * FROM table;
+DROP table table_info;
+SELECT * FROM table_info;
 
 -- Sort in order
-SELECT * FROM table ORDER BY Pizza DESC;
+SELECT * FROM table_info ORDER BY Pizza ASC;
